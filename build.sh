e6e2e30690365b8925dc889e9aaa896371b2e078
tukiyo3 <tukiyo3@gmail.com>
2019-10-31 09:13:21 +0900

    update

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export BUILD="docker build . --no-cache=false -f "
 # $BUILD Dockerfile.centos8-gcc740 -t tukiyo/centos8-gcc:gcc740
 # $BUILD Dockerfile.centos8-gcc830 -t tukiyo/centos8-gcc:gcc830
 # $BUILD Dockerfile.centos8-gcc920 -t tukiyo/centos8-gcc:gcc920
-$BUILD Dockerfile.centos8-4.9 -t tukiyo/centos8-gcc:4.9
+$BUILD Dockerfile.centos8-gcc447 -t tukiyo/centos8-gcc:centos8-gcc447
 
